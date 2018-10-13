2157179e6c889a2ad88906545a73512f1ecb307e
TOMas Marinak <mar0612@vsb.cz>
2018-10-13 16:31:02 +0200

    added basics of db connection

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,8 @@
+import com.project.config.Connection;
+
 public class Application {
     public  static  void  main(String[]args){
-
+        Connection connection = null;
+        connection.connect();
     }
 }
